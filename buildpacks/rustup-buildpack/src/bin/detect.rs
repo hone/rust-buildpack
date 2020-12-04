134707e34d55df26b0c33a0729b283e72fe8ee9e
Terence Lee <hone02@gmail.com>
2020-12-04 17:21:06 -0600

    detect now handles loading user env

--- buildpacks/rustup-buildpack/src/bin/detect.rs
+++ buildpacks/rustup-buildpack/src/bin/detect.rs
@@ -4,13 +4,16 @@ use libcnb_rs::{
 };
 
 const ARGS_EXIT_CODE: i32 = 101;
+const BUILDPLAN_EXIT_CODE: i32 = 102;
 
 fn main() {
-    if let Some(detect) = Detect::from_args(std::env::args()) {
+    if let Ok(Some(detect)) = Detect::from_args(std::env::args()) {
         let mut build_plan = BuildPlan::new();
         build_plan.provides.push(Provide::new("rust"));
 
-        detect.pass();
+        detect
+            .pass(Some(&build_plan))
+            .unwrap_or_else(|_| detect.error(BUILDPLAN_EXIT_CODE));
     } else {
         eprintln!("Did not get proper detect arguments");
         std::process::exit(ARGS_EXIT_CODE);
--- libcnb-rs/src/detect.rs
+++ libcnb-rs/src/detect.rs
@@ -1,5 +1,10 @@
-use crate::build_plan::BuildPlan;
-use std::{fs, path::PathBuf, process};
+use crate::{build_plan::BuildPlan, error::Error};
+use std::{
+    collections::HashMap,
+    fs,
+    path::{Path, PathBuf},
+    process,
+};
 
 const PASS_EXIT_CODE: i32 = 0;
 const FAIL_EXIT_CODE: i32 = 100;
@@ -10,30 +15,43 @@ pub struct Detect {
     pub platform_path: PathBuf,
     /// Build Plan (TOML)
     build_plan_path: PathBuf,
+    /// User Environment
+    pub user_env: HashMap<String, String>,
 }
 
 impl Detect {
-    pub fn new(platform: impl Into<PathBuf>, build_plan: impl Into<PathBuf>) -> Self {
-        Detect {
-            platform_path: platform.into(),
+    pub fn new(
+        platform: impl Into<PathBuf>,
+        build_plan: impl Into<PathBuf>,
+    ) -> Result<Self, Error> {
+        let platform_path = platform.into();
+        let env = Self::read_env(&platform_path)?;
+
+        Ok(Detect {
+            platform_path,
             build_plan_path: build_plan.into(),
-        }
+            user_env: env,
+        })
     }
 
-    pub fn from_args(mut args: impl Iterator<Item = String>) -> Option<Self> {
+    pub fn from_args(mut args: impl Iterator<Item = String>) -> Result<Option<Self>, Error> {
         match (args.next(), args.next()) {
-            (Some(platform), Some(build_plan)) => Some(Self::new(platform, build_plan)),
-            _ => None,
+            (Some(platform), Some(build_plan)) => Ok(Some(Self::new(platform, build_plan)?)),
+            _ => Ok(None),
         }
     }
 
-    pub fn write_buildplan(&self, build_plan: &BuildPlan) -> Result<(), crate::error::Error> {
+    pub fn write_buildplan(&self, build_plan: &BuildPlan) -> Result<(), Error> {
         fs::write(&self.build_plan_path, toml::to_string(build_plan)?)?;
 
         Ok(())
     }
 
-    pub fn pass(&self) {
+    pub fn pass(&self, build_plan: Option<&BuildPlan>) -> Result<(), Error> {
+        if let Some(build_plan) = build_plan {
+            fs::write(&self.build_plan_path, toml::to_string(build_plan)?)?;
+        }
+
         process::exit(PASS_EXIT_CODE);
     }
 
@@ -44,5 +62,29 @@ impl Detect {
     /// error code is 1-99 or 101+
     pub fn error(&self, code: i32) {
         process::exit(code);
+    }
+
+    /// Read User Environment into a HashMap. Returns an empty HashMap if the path does not exist.
+    fn read_env(path: impl AsRef<Path>) -> Result<HashMap<String, String>, Error> {
+        let path = path.as_ref();
+        let mut env = HashMap::new();
+
+        if path.is_dir() {
+            for entry in fs::read_dir(path)? {
+                let entry = entry?;
+                let path = entry.path();
+
+                let key_os = match path.file_stem() {
+                    Some(key_os) => key_os,
+                    None => continue,
+                };
+                if let Some(key) = key_os.to_str() {
+                    let value = fs::read_to_string(&path)?;
+                    env.insert(key.to_string(), value);
+                }
+            }
+        }
+
+        Ok(env)
     }
 }
